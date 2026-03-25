62b17c4a1e0ef24675dd3bf49f0b1cdf8eed9b1e
toosiike <toosii042@gmail.com>
2026-03-25 20:14:54 +0300

    fix: use serverExternalPackages (Next.js 14 API) + add movie image hostnames

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'files.catbox.moe' },
-      { protocol: 'https', hostname: 'i.ytimg.com' },
-      { protocol: 'https', hostname: 'api.giftedtech.co.ke' },
-    ],
-  },
-  experimental: {
-    serverComponentsExternalPackages: ['gifted-baileys', 'pino'],
-  },
-}
+  const nextConfig = {
+    images: {
+      remotePatterns: [
+        { protocol: 'https', hostname: 'files.catbox.moe' },
+        { protocol: 'https', hostname: 'i.ytimg.com' },
+        { protocol: 'https', hostname: 'api.giftedtech.co.ke' },
+        { protocol: 'https', hostname: 'img.movieapi.xcasper.space' },
+        { protocol: 'https', hostname: 'movieapi.xcasper.space' },
+      ],
+    },
+    serverExternalPackages: ['gifted-baileys', 'pino'],
+  }
 
-module.exports = nextConfig
+  module.exports = nextConfig
+  
