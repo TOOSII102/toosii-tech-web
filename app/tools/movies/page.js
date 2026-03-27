cf1c1ac806316dd720413c098b55d12c909efd8a
toosiike <toosii042@gmail.com>
2026-03-27 16:35:55 +0300

    Fix TV embed: restore vidsrc.to, add allow-popups so player can load streams
    Keep allow-top-navigation blocked so main page cannot be redirected by ads

--- app/tools/movies/page.js
+++ app/tools/movies/page.js
@@ -475,12 +475,11 @@ function DetailModal({ movie, onClose }) {
                       {seasons.length > 0 && imdbId ? (
                         <iframe
                           key={`vidsrc-${imdbId}-${activeSeason}-${activeEpNum}`}
-                          src={`https://vidsrc.me/embed/tv?imdb=${imdbId}&season=${activeSeason}&episode=${activeEpNum}`}
+                          src={`https://vidsrc.to/embed/tv/${imdbId}/${activeSeason}/${activeEpNum}`}
                           className="mv-video"
                           allowFullScreen
                           allow="autoplay; fullscreen; picture-in-picture"
-                          sandbox="allow-scripts allow-same-origin allow-forms allow-fullscreen allow-presentation allow-orientation-lock"
-                          referrerPolicy="no-referrer"
+                          sandbox="allow-scripts allow-same-origin allow-forms allow-fullscreen allow-presentation allow-orientation-lock allow-popups"
                           style={{ border: 'none' }}
                           title={`${d.title} S${activeSeason}E${activeEpNum}`}
                         />
